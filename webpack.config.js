579bd50f70115c9562a99fb5ab59eb51a6f16f2f
odeadglaz <oded.goldglas+git@fiverr.com>
2022-02-02 13:55:54 +0200

    Babel support for es5.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
 module.exports = {
 	mode: 'production',
 	entry: './index.js',
+	target: ['web', 'es5'],
 	output: {
 		filename: 'index.js',
 		library: {
 			type: 'commonjs2',
-		},
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}
+		]
 	},
 	devtool: 'source-map',
 };
